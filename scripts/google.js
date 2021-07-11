6ef9ac5eb68d6024c3c103c50a0156632499b0bf
aNeemic <tpow1113@gmail.com>
2021-07-11 09:15:34 -0400

    made buttons clickable and adjusted their colors. Added profile picture and color to footer

---
+++ scripts/google.js
@@ -0,0 +1,33 @@
+document.getElementById("about").onclick = function () {
+    location.href = "https://about.google/?fg=1&utm_source=google-US&utm_medium=referral&utm_campaign=hp-header";
+}
+document.getElementById("store").onclick = function () {
+    location.href = "https://store.google.com/US/?utm_source=hp_header&utm_medium=google_ooo&utm_campaign=GS100042&hl=en-US";
+};
+document.getElementById("gmail").onclick = function () {
+    location.href = "https://mail.google.com/";
+};
+document.getElementById("images").onclick = function () {
+    location.href = "https://www.google.com/imghp?hl=en&authuser=0&ogbl";
+};
+document.getElementById("signIn").onclick = function () {
+    location.href = "https://github.com/aNeemic";
+};
+document.getElementById("advertising").onclick = function () {
+    location.href = "https://ads.google.com/intl/en_us/home/?subid=ww-ww-et-g-awa-a-g_hpafoot1_1!o2m--ahpm-0000000078-0000000001&utm_source=google.com&utm_medium=referral&utm_campaign=google_hpafooter&fg=1#!/";
+};
+document.getElementById("business").onclick = function () {
+    location.href = "https://smallbusiness.withgoogle.com/?subid=us-en-et-g-awa-a-g_hpbfoot1_1!o2&utm_source=google&utm_medium=ep&utm_campaign=google_hpbfooter&utm_content=google_hpbfooter&gmbsrc=us-en_US-et-gs-z-gmb-s-z-u~sb-g4sb_srvcs-u#!/";
+};
+document.getElementById("how").onclick = function () {
+    location.href = "https://www.google.com/search/howsearchworks/?fg=1";
+};
+document.getElementById("privacy").onclick = function () {
+    location.href = "https://policies.google.com/privacy?hl=en&fg=1";
+};
+document.getElementById("terms").onclick = function () {
+    location.href = "https://policies.google.com/terms?hl=en&fg=1";
+};
+document.getElementById("settings").onclick = function () {
+    location.href = "www.yoursite.com";
+};
